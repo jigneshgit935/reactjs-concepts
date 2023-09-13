70a8e9e16a9b3d62a3b1b8b03ce21e164a54b359
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-14 02:05:50 +0530

    update

--- bg-changer/bg-changer/src/App.jsx
+++ bg-changer/bg-changer/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [color, setColor] = useState('olive');
+  const [color, setColor] = useState('');
   return (
     <div
       className="w-full h-screen duration-200"
