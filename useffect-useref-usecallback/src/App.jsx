97deb52cbfbd8b58a2cb01b2fc69716b2c4090fb
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-14 03:11:17 +0530

    commited

--- useffect-useref-usecallback/src/App.jsx
+++ useffect-useref-usecallback/src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState, useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [characters, setCharacters] = useState(false);
+  const [password, setPassword] = useState('');
 
+  const passRef = useRef(null);
+
+  const copyPasswordToClip = useCallback(() => {
+    passRef.current?.select();
+    // passRef.current?.setSelectionRange(0, 20);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if (number) str += '0123456789';
+    if (characters) str += '!@#$%^&*-_+=[]{}~`';
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, number, characters, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, number, characters, passwordGenerator]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-md mx-auto rounded-lg shadow-md px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-4xl text-center text-white my-3">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            value={password}
+            ref={passRef}
+          />
+
+          <button
+            onClick={copyPasswordToClip}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5  shrink-0"
+          >
+            Copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              className="cursor-pointer"
+              min={6}
+              max={20}
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label htmlFor="">Length: {length}</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              id="numberInput"
+              defaultChecked={number}
+              onChange={() => setNumber((prev) => !prev)}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              id="characterInput"
+              defaultChecked={characters}
+              onChange={() => setCharacters((prev) => !prev)}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
